45d9b880ea3e54593701be17dcae07606b36ab4a
Remynijsten <99012252@mydavinci.nl>
2021-09-21 21:55:17 +0200

    History mode route testing

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
 const app = express();
+const history = require("connect-history-api-fallback");
+
+app.use(history());
+
 app.use(serveStatic(path.join(__dirname, "dist")));
+
 const port = process.env.PORT || 80;
 app.listen(port);
