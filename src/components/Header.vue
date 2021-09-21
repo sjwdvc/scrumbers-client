bb57ffcbc022dbfb3e409f979d48da2e64de8a48
Remynijsten <99012252@mydavinci.nl>
2021-09-21 20:38:06 +0200

    Initial Commit

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+	<h2>{{ content }}</h2>
+</template>
+
+<script>
+export default {
+	name: "Header",
+	props: {
+		content: String
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<div>HALLO</div>
+		<Header content="WELKOM" />
+	</div>
+</template>
+
+<script>
+
+import Header from "@/components/Header";
+
+export default {
+  	name: "HelloWorld",
+	components: {
+		Header
+	},
+  	props: {
+    	msg: String
+  	}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
