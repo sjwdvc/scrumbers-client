a48a6ca37729845bd7a80678baec65da46d82ed0
Remynijsten <99012252@mydavinci.nl>
2021-09-22 07:40:29 +0200

    Added animation scss & jquery dependency

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,33 +1,34 @@
 <template>
-	<button :type="$props.type">{{ $props.content | capitalize }}</button>
+  <button :type="$props.type">{{ $props.content | capitalize }}</button>
 </template>
 
 <script>
 export default {
-	name : "Button",
-	props: {
-		type: String,
-		content: String
-	}
-}
+  name: "Button",
+  props: {
+    type: String,
+    content: String
+  }
+};
 </script>
 
 <style scoped lang="scss">
-	@import "../../src/scss/main.scss";
+@import "../../src/scss/main.scss";
 
-	button{
-		background-color: $gold;
-		color: $blue-dark;
-		font-weight: 800;
-		padding: 1rem;
-		text-align: center;
-		border-radius: 10px;
-		border: none;
-		border-bottom: 5px solid darken($gold, 10%);
-		transition: 0.3s ease;
-		cursor: pointer;
-		&:hover{
-			transform: translateY(-5px);
-		}
-	}
+button {
+  background-color: $gold;
+  color: $blue-dark;
+  font-weight: 800;
+  padding: 1rem;
+  text-align: center;
+  border-radius: 10px;
+  border: none;
+  border-bottom: 5px solid darken($gold, 10%);
+  transition: 0.3s ease;
+  min-width: 150px;
+  cursor: pointer;
+  &:hover {
+    transform: translateY(-5px);
+  }
+}
 </style>
