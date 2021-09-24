8adcf46c83d338c8385026579e9d44a2ccb417c4
RickSnijders <99060582@mydavinci.nl>
2021-09-24 11:37:02 +0200

    Added login frontend
    Added login frontend

--- src/router.js
+++ src/router.js
@@ -2,25 +2,29 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home";
 import Register from "./views/Register";
-
+import Login from "./views/Login";
 
 Vue.use(Router);
 
 export default new Router({
-    mode: "history",
-    base: process.env.BASE_URL,
-    routes: [
-        {
-            path: "/",
-            component: Home
-        },
-        {
-            path: "/register",
-            component: Register
-        },
-        {
-            path: "/about",
-            component: () => import("./views/About.vue")
-        }
-    ]
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      component: Home
+    },
+    {
+      path: "/register",
+      component: Register
+    },
+    {
+      path: "/login",
+      component: Login
+    },
+    {
+      path: "/about",
+      component: () => import("./views/About.vue")
+    }
+  ]
 });
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,13 +1,88 @@
 <template>
-	<section class="login">
-		<Logo />
-	</section>
+  <section class="login">
+    <Logo />
+    <div class="interface">
+      <DisplayHeader content="INLOGGEN" />
+      <form action="" class="login-form" @submit.prevent="submitData">
+        <Input
+          type="email"
+          name="email"
+          placeholder="Email"
+          required
+          v-model="form.email"
+        />
+        <Input
+          type="password"
+          name="password"
+          placeholder="Wachtwoord"
+          required
+          v-model="form.password"
+        />
+        <Button type="submit" content="Inloggen" />
+      </form>
+    </div>
+    <p>
+      Heb je nog geen account ?
+      <router-link to="register">Klik hier om te registreren</router-link>
+    </p>
+  </section>
 </template>
 
 <script>
-	import Logo from "../components/Logo";
-	export default {
-		name: "home",
-		components: {Logo}
-	};
+import axios from "axios";
+import DisplayHeader from "../components/text/DisplayHeader";
+import Input from "../components/Input";
+import Button from "../components/Button";
+
+export default {
+  name: "login",
+  components: { Button, DisplayHeader, Input },
+  data() {
+    return {
+      form: {
+        email: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    // Submit the formdata to the server url defined in main.js using a post request
+    submitData() {
+      axios
+        .post(this.$server, this.form)
+        .then()
+        .catch(function(error) {
+          console.log(error.response.data.errors);
+        });
+    }
+  }
+};
 </script>
+
+<style scoped lang="scss">
+@import "../../src/scss/main";
+
+.login {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  h1 {
+    margin: 0 auto 50px;
+    text-align: center;
+  }
+  &-form {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    button {
+      margin: 25px auto 0;
+    }
+  }
+  p {
+    color: $white;
+    text-align: center;
+    margin-top: 25px;
+  }
+}
+</style>
