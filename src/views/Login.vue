fbd3b227bc108fae0cdb2315aa1abaa9ba104da0
Remynijsten <99012252@mydavinci.nl>
2021-09-27 09:58:46 +0200

    Added front end notifications for login & register forms

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,31 +1,32 @@
 <template>
-  <section class="login">
-    <Logo />
-    <div class="interface">
-      <DisplayHeader content="INLOGGEN" />
-      <form action="" class="login-form" @submit.prevent="submitData">
-        <Input
-          type="email"
-          name="email"
-          placeholder="Email"
-          required
-          v-model="form.email"
-        />
-        <Input
-          type="password"
-          name="password"
-          placeholder="Wachtwoord"
-          required
-          v-model="form.password"
-        />
-        <Button type="submit" content="Inloggen" />
-      </form>
-    </div>
-    <p>
-      Heb je nog geen account ?
-      <router-link to="register">Klik hier om te registreren</router-link>
-    </p>
-  </section>
+	<section class="login">
+		<Logo />
+		<div class="interface">
+			<DisplayHeader content="INLOGGEN" />
+			<form action="" class="login-form" @submit.prevent="submitData">
+				<Input
+					type="email"
+					name="email"
+					placeholder="Email"
+					required
+					v-model="form.email"
+				/>
+				<Input
+					type="password"
+					name="password"
+					placeholder="Wachtwoord"
+					required
+					v-model="form.password"
+				/>
+				<p>{{ error }}</p>
+				<Button type="submit" content="Inloggen" />
+			</form>
+		</div>
+		<p>
+			Heb je nog geen account ?
+			<router-link to="register">Klik hier om te registreren</router-link>
+		</p>
+	</section>
 </template>
 
 <script>
@@ -35,54 +36,79 @@ import Input from "../components/Input";
 import Button from "../components/Button";
 
 export default {
-  name: "login",
-  components: { Button, DisplayHeader, Input },
-  data() {
-    return {
-      form: {
-        email: "",
-        password: ""
-      }
-    };
-  },
-  methods: {
-    // Submit the formdata to the server url defined in main.js using a post request
-    submitData() {
-      axios
-        .post(this.$server, this.form)
-        .then()
-        .catch(function(error) {
-          console.log(error.response.data.errors);
-        });
-    }
-  }
+	name: "login",
+	components: { Button, DisplayHeader, Input },
+	data() {
+		return {
+			form: {
+				email: "",
+				password: ""
+			},
+			error: ""
+		};
+	},
+	methods: {
+		// Submit the formdata to the server url defined in main.js using a post request
+		submitData() {
+			axios.post(this.$server + 'login', this.form)
+			.then(res => {
+				this.error = "";
+
+				document.querySelectorAll("input").forEach(input => {
+					input.style.border = "none";
+				});
+
+				if (res.data.error) {
+
+
+					// Highlight the input field containing the error
+					res.data.field !== "global"
+						? (document.querySelector(
+						"input[name=" + res.data.field + "]"
+						).style.border = "2px solid #A03A3C")
+						: "";
+
+					// Comment the error under the form
+					this.error = res.data.error;
+				} else {
+					// document.querySelector("button").setAttribute("disabled", "");
+					this.error = "Inloggen...";
+
+					// Router push to logged-in homescreen
+				}
+			})
+			.catch(function(error) {
+				console.log(error);
+			});
+		}
+	}
 };
 </script>
-
+	
 <style scoped lang="scss">
 @import "../../src/scss/main";
 
 .login {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  h1 {
-    margin: 0 auto 50px;
-    text-align: center;
-  }
-  &-form {
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    button {
-      margin: 25px auto 0;
-    }
-  }
-  p {
-    color: $white;
-    text-align: center;
-    margin-top: 25px;
-  }
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	h1 {
+		margin: 0 auto 50px;
+		text-align: center;
+	}
+	&-form {
+		display: flex;
+		justify-content: center;
+		flex-direction: column;
+		button {
+			margin: 25px auto 0;
+		}
+	}
+	p {
+		color: $white;
+		text-align: center;
+		margin-top: 25px;
+	}
 }
 </style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -3,76 +3,125 @@
 		<div class="interface">
 			<DisplayHeader content="WELKOM" />
 			<form action="" class="register-form" @submit.prevent="submitData">
-				<Input type="text" name="name" placeholder="Naam" required v-model="form.name"/>
-				<Input type="email" name="email" placeholder="Email" required v-model="form.email"/>
-				<Input type="password" name="password" placeholder="Wachtwoord" required v-model="form.password"/>
-				<Button type="submit" content="Registreren" />
+				<Input
+					type="text"
+					name="name"
+					placeholder="Naam"
+					required
+					v-model="form.name"
+					ref="name"
+				/>
+				<Input
+					type="email"
+					name="email"
+					placeholder="Email"
+					required
+					v-model="form.email"
+					ref="email"
+				/>
+				<Input
+					type="password"
+					name="password"
+					placeholder="Wachtwoord"
+					required
+					v-model="form.password"
+					ref="password"
+				/>
+				<p>{{ error }}</p>
+				<Button type="submit" content="Registreren" ref="button" />
 			</form>
 		</div>
 
-		<p>Heb je al een account ? <router-link to="login">Klik hier om in te loggen</router-link></p>
+		<p class="login-link">
+			Heb je al een account ?
+			<router-link to="login">Klik hier om in te loggen</router-link>
+		</p>
 	</div>
 </template>
 
 <script>
-
-import axios from 'axios';
+import axios from "axios";
 import DisplayHeader from "../components/text/DisplayHeader";
 import Input from "../components/Input";
 import Button from "../components/Button";
 
 export default {
-	name : "Register",
-	components : {Button, DisplayHeader, Input},
-	data () {
+	name: "Register",
+	components: { Button, DisplayHeader, Input },
+	data() {
 		return {
 			form: {
-				name: '',
-				email: '',
-				password: ''
-			}
-		}
+				name: "",
+				email: "",
+				password: ""
+			},
+			error: ""
+		};
 	},
-	methods : {
-		changeValue(e)
-		{
-			console.log(e)
-		},
-		submitData()
-		{
-			axios.post(this.$server, this.form)
-				.then(data => console.log(data))
-				.catch(function (error) { console.log(error.response.data.errors); });
+	methods: {
+		// Submit the formdata to the server url defined in main.js using a post request
+		submitData() {
+			axios.post(this.$server + '/register', this.form)
+			 .then(res => {
+				 // Initiate/reset an empty error message
+				 this.error = "";
+
+				 // Reset all input border styling to none
+				 document.querySelectorAll('input').forEach(input => {
+					 input.style.border = 'none';
+				 });
+
+				 if (res.data.error) {
+					 // Highlight the input field containing the error
+					 res.data.field !== 'global'
+						 ? (document.querySelector(
+						 'input[name=' + res.data.field + ']'
+						 ).style.border = '2px solid #A03A3C')
+						 : '';
+
+					 // Comment the error under the form
+					 this.error = res.data.error;
+				 } else {
+					 document.querySelector('button').setAttribute('disabled', '');
+					 this.error = 'Inloggen...';
+
+					 // Router push to logged-in homescreen
+				 }
+			 })
+			 .catch(function(error) {
+					 console.log(error);
+			 });
 		}
 	}
-}
+};
 </script>
 
-<style scoped lang="scss">
+<style scoped lang='scss'>
+@import '../../src/scss/main';
 
-	@import "../../src/scss/main";
-
-	.register{
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, -50%);
-		h1{
-			margin: 0 auto 50px;
-			text-align: center;
-		}
-		&-form{
-			display: flex;
-			justify-content: center;
-			flex-direction: column;
-			button{
-				margin: 25px auto 0;
-			}
-		}
-		p{
-			color: $white;
-			text-align: center;
-			margin-top: 25px;
+.register {
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	h1 {
+		margin: 0 auto 50px;
+		text-align: center;
+	}
+	&-form {
+		display: flex;
+		justify-content: center;
+		flex-direction: column;
+		button {
+			margin: 25px auto 0;
 		}
 	}
+	p {
+		color: $white;
+		text-align: center;
+	}
+	.login-link {
+		margin-top: 25px;
+	}
+}
 </style>
