4951b9120860bfcbcd62d1adabb9363a2b04b5d9
Remynijsten <99012252@mydavinci.nl>
2021-09-29 13:02:02 +0200

    Removed header

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -6,7 +6,6 @@ export default function checkLogin() {
         withCredentials: true,
         headers:
             {
-                'Access-Control-Allow-Origin': "*",
                 Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6IlJlbXkiLCJ1c2VyRW1haWwiOiJpbmZvQHNlYXNpZGVjb2RpbmcuY29tIiwiaWF0IjoxNjMyOTA5MTEzfQ.nWKsruZQ7z_ewkD1s7bPyp6XlQ8XjDPQxG_sN0FWkdA"
             }})
 }
