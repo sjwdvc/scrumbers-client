765ad5a1a1227c07f2c9faa6276d3ea11decf4d1
Remynijsten <99012252@mydavinci.nl>
2021-10-11 09:16:03 +0200

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,13 @@
-// export const SERVER = 'https://scrumbers-server.herokuapp.com/api/'
-export const SERVER = 'https://localhost:5555/api/'
+// export const SERVER  = 'https://scrumbers-server.herokuapp.com/api/'
+export const SERVER     = 'https://localhost:5555/api/'
 
-// export const CLIENT = 'https://scrumbers-client.herokuapp.com/'
-export const CLIENT = 'http://localhost:8081'
+// export const CLIENT  = 'https://scrumbers-client.herokuapp.com'
+export const CLIENT     = 'https://localhost:8081'
+
+// export const SOCKET     = io('https://scrumbers-server.herokuapp.com');
+export const SOCKET     = io('https://localhost:5555/');
+
+export let USER         = {}
+
+export let TOKEN        = localStorage.getItem('TOKEN')
+
