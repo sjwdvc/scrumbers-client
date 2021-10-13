5664a936c1e13d0e687e933eb71f7849d15086c3
Gi0vanniM <g.l.mierop@gmail.com>
2021-10-13 15:09:03 +0200

    fixed copy button getting 'undefined'

--- src/App.vue
+++ src/App.vue
@@ -42,7 +42,7 @@ export default {
 		{
 			this.$refs.inputwrapper.classList.add('animate__rubberBand')
 			this.$toast.open({message: 'Link gekopieerd!', type: "success", position: "top-right"});
-			navigator.clipboard.writeText(this.link);
+			navigator.clipboard.writeText(this.shareLink.url);
 		}
 	}
 
