73d938c92aa866a9ab46ccf2e1a3c9eea7f1f091
Gi0vanniM <g.l.mierop@gmail.com>
2021-10-27 12:10:00 +0200

    redirect to home instead of create-room

--- src/main.js
+++ src/main.js
@@ -44,12 +44,7 @@ router.beforeEach((to, from, next) => {
 
                 // If user logged in and trying to access login screen
                 case (data.data.login && to.name === 'login'):
-                    next({name: 'createroom'})
-                    break;
-
-                // If user logged in, change new home to create room
-                case (data.data.login && to.name === 'home'):
-                    next({name: 'createroom'})
+                    next({ name: 'home' })
                     break;
 
                 default: next()
