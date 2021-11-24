7aa1cbf3fd581898249e4c0ca27ea7b73d097186
Gi0vanniM <g.l.mierop@gmail.com>
2021-11-24 12:29:19 +0100

    added number and assignee to votespopup

--- src/components/VotesPopup.vue
+++ src/components/VotesPopup.vue
@@ -4,9 +4,23 @@
             <DisplayHeader content="FEATURE RESULT" />
             <p id="feature">Feature</p>
             <p id="feature-name">{{ feature.name }}</p>
-            <br><br>
-            <p>Number</p>
 
+            <br /><br />
+
+            <p>Number</p>
+            <!-- Number icon -->
+            <div id="number">
+                <p>
+                    {{ feature.number || "5" }}
+                </p>
+            </div>
+
+            <br /><br />
+
+            <p>Assigned to</p>
+            <div id="assignee">
+                <p>{{ feature.assignee || "Assignee name" }}</p>
+            </div>
         </div>
     </div>
 </template>
@@ -35,11 +49,12 @@ h1 {
 p {
     font-size: 20px;
     color: white;
+    font-weight: lighter;
 }
 .votespopup {
     z-index: 100;
     position: absolute;
-    top: 10%;
+    top: 30%;
     left: 50%;
     -webkit-transform: translate(-50%, -50%);
     transform: translate(-50%, -50%);
@@ -49,5 +64,31 @@ p {
 #feature-name {
     font-size: 24px;
     font-weight: bold;
+}
+
+#number {
+    border-radius: 10px;
+    background-color: white;
+    height: 10em;
+    width: 10em;
+    margin: 10px auto;
+    display: flex;
+    align-items: center; /* Vertical center alignment */
+    justify-content: center; /* Horizontal center alignment */
+}
+#number p {
+    color: black;
+    font-size: 5em;
+}
+
+#assignee {
+    border-radius: 10px;
+    background-color: rgba(53, 52, 52, 0.349);
+    height: 5em;
+    width: 20em;
+    margin: 10px auto;
+    display: flex;
+    align-items: center; /* Vertical center alignment */
+    justify-content: center; /* Horizontal center alignment */
 }
 </style>
