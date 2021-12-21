f9793ff7bc69459cd7d7cf36793d7deb434787d6
Rick <rlugtigheid77@gmail.com>
2021-12-21 11:50:04 +0100

    Fixed / in production url

--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,7 @@ let host;
 if (process.env.NODE_ENV && process.env.NODE_ENV == 'development')
     host = { server: 'https://localhost:5555', client: 'https://localhost:8080' };
 else
-    host = { server: 'https://scrumbers-server.herokuapp.com/', client: 'https://scrumbers-client.herokuapp.com' };
+    host = { server: 'https://scrumbers-server.herokuapp.com', client: 'https://scrumbers-client.herokuapp.com' };
 export const SERVER  = host.server + '/api/'
 //
 //export const CLIENT     = 'https://scrumbers-client.herokuapp.com'
