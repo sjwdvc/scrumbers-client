d579628336f6f060b44b42ce925aae4b17726db4
Rick <rlugtigheid77@gmail.com>
2021-12-21 11:46:37 +0100

    Fixed / in url

--- src/constants.js
+++ src/constants.js
@@ -2,10 +2,10 @@ import {io} from "socket.io-client";
 
 let host;
 if (process.env.NODE_ENV && process.env.NODE_ENV == 'development')
-    host = { server: 'https://localhost:5555/', client: 'https://localhost:8080/' };
+    host = { server: 'https://localhost:5555', client: 'https://localhost:8080' };
 else
     host = { server: 'https://scrumbers-server.herokuapp.com/', client: 'https://scrumbers-client.herokuapp.com' };
-export const SERVER  = host.server + 'api/'
+export const SERVER  = host.server + '/api/'
 //
 //export const CLIENT     = 'https://scrumbers-client.herokuapp.com'
 export const CLIENT     = host.client
