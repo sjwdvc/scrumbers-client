01f6591c4c57ae92398cd9f0c616520cd85c3e2c
Gi0vanniM <g.l.mierop@gmail.com>
2022-01-10 21:06:56 +0100

    result popup is now 1 minute long, and added a dismiss button which closes the popup.

--- src/components/VotesPopup.vue
+++ src/components/VotesPopup.vue
@@ -1,70 +1,90 @@
 <template>
-	<div class="votespopup" :class="{'d-none' : !$parent.votes.visible}">
-		<div class="interface">
-			<DisplayHeader content="FEATURE RESULT" />
-			<p id="feature">Feature</p>
-			<p id="feature-name">{{ $parent.votes.feature }}</p>
-			
-			<br><br>
-			
-			<p>Number</p>
-			<!-- Number icon -->
-			<div id="number">
-				<p>
-					{{ $parent.votes.number || "-" }}
-				</p>
-			</div>
-			
-			<br><br>
-			
-			<p>Assigned to</p>
-			<div id="assignee">
-				<p>{{ $parent.votes.member || "" }}</p>
-			</div>
-			
-			<div class="progress">
-				<div class="progress-bar"></div>
-			</div>
-		</div>
-	</div>
+    <div class="votespopup" :class="{ 'd-none': !$parent.votes.visible }">
+        <div class="interface">
+            <DisplayHeader content="FEATURE RESULT" />
+            <button v-on:click="dismissVotesPopup" id="dismissButton">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="32"
+                    height="32"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="#ffffff"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                >
+                    <line x1="18" y1="6" x2="6" y2="18"></line>
+                    <line x1="6" y1="6" x2="18" y2="18"></line>
+                </svg>
+            </button>
+            <p id="feature">Feature</p>
+            <p id="feature-name">{{ $parent.votes.feature }}</p>
+
+            <br /><br />
+
+            <p>Number</p>
+            <!-- Number icon -->
+            <div id="number">
+                <p>
+                    {{ $parent.votes.number || "-" }}
+                </p>
+            </div>
+
+            <br /><br />
+
+            <p>Assigned to</p>
+            <div id="assignee">
+                <p>{{ $parent.votes.member || "" }}</p>
+            </div>
+
+            <div class="progress">
+                <div class="progress-bar"></div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 import DisplayHeader from "../components/text/DisplayHeader";
-import EVENTBUS from '../eventbus';
+import EVENTBUS from "../eventbus";
 
 export default {
-	name: "VotesPopup",
-	components: {
-		DisplayHeader,
-	},
-	methods: {
-		startTimer() {
-			let timer = 1000;
-			let interval = setInterval(() => {
-				timer--;
-				this.updateProgressBar(timer);
-				
-				if (timer <= 0)
-				{
-					this.$parent.votes.visible = false
-					this.$parent.session.visible = true;
-					clearInterval(interval);
-				}
-			}, 1);
-		},
-		
-		updateProgressBar(time) {
-			document.querySelector('.progress-bar').style.width = (time / 10) + "%";
+    name: "VotesPopup",
+    components: {
+        DisplayHeader,
+    },
+    methods: {
+        startTimer() {
+            let timer = 1000;
+            let interval = setInterval(() => {
+                timer--;
+                this.updateProgressBar(timer);
+
+                if (timer <= 0) {
+                    this.$parent.votes.visible = false;
+                    this.$parent.session.visible = true;
+                    clearInterval(interval);
+                }
+            }, 60);
+        },
+
+        updateProgressBar(time) {
+            document.querySelector(".progress-bar").style.width =
+                time / 10 + "%";
+        },
+
+		dismissVotesPopup() {
+			this.$parent.votes.visible = false;
+			this.$parent.session.visible = true;
 		}
-	},
-	mounted()
-	{
-		EVENTBUS.$on('results', () => {
-			this.startTimer();
-			this.$parent.votes.visible = true;
-			this.$parent.session.visible = false;
-		})
-	}
+    },
+    mounted() {
+        EVENTBUS.$on("results", () => {
+            this.startTimer();
+            this.$parent.votes.visible = true;
+            this.$parent.session.visible = false;
+        });
+    },
 };
 </script>
